7514a9a8069df7aba43c247281ef478d680fb28d
etqjuyreal <realokman1@gmail.com>
2025-03-27 20:44:18 +0000

    Update index.tsx

--- plugins/create-webhooks/src/index.tsx
+++ plugins/create-webhooks/src/index.tsx
@@ -1,47 +1,18 @@
-import { findByName, findByProps } from "@vendetta/metro";
-import { stylesheet, React, ReactNative, i18n } from "@vendetta/metro/common";
-import { instead} from "@vendetta/patcher";
+import { before } from "vendetta";
+import { findByProps } from "vendetta/metro";
 
-let patches = [];
+const MessageModule = findByProps("sendMessage", "editMessage");
 
-const {View} = ReactNative;
-
-const ConnectedWebhooksOverview = findByName("ConnectedWebhooksOverview", false);
-const Button = findByName("Button", false).default;
-const { create: createWebhook } = findByProps("update", "create", "fetchForChannel");
-const {getChannel} = findByProps("getChannel");
-
-const Styles = stylesheet.createThemedStyleSheet({
-    'container': {
-        'flex': 1,
-        'paddingBottom': 32
-    },
-    'button': {
-        'maxHeight': 32,
-        'margin': 16
-    }
-})
-
-function onLoad() {   
-    patches.push(instead("default", ConnectedWebhooksOverview, (args, orig) => {
-        const channel = getChannel(args[0]?.channelId)
-        return (<View style={Styles.container}>
-            {orig?.(...args)}
-            <Button color="brand" style={Styles.button}
-                text={i18n?.Messages?.WEBHOOK_CREATE || "Create Webhook"}
-                onPress={() => {
-                    createWebhook?.(channel?.guild_id, channel?.id)
-                }}
-            />
-        </View>)
-    }))
+function change(str: string): string {
+    return str.replace(/\w+/g, (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
 }
 
-export default {
-    onLoad,
-    onUnload: () => {
-        for (const unpatch of patches) {
-            unpatch();
-        };
+const unpatch = before("sendMessage", MessageModule, (args) => {
+    if (args[1]?.content) {
+        args[1].content = change(args[1].content);
     }
+});
+
+export function onUnload() {
+    unpatch();
 }
